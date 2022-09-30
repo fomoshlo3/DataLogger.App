1eaed65d311a556e01be5418d70fe7b7922121fa
Hannes Kasel <hanneskasel@gmail.com>
2022-09-30 08:15:06 +0200

    corrected Autocorrect Shit

--- DataLogger.App/DataLogger.form.Designer.cs
+++ DataLogger.App/DataLogger.form.Designer.cs
@@ -89,7 +89,6 @@
             this.Controls.Add(this.btn_CSVLoad);
             this.Name = "MainForm";
             this.Text = "MainForm";
-            this.Load += new System.EventHandler(this.MainForm_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
 
